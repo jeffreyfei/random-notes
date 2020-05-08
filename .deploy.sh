a92b2828201191447f75c37fe00e20f838027a6d
Jeffrey Fei <jeffreyfei@msn.com>
2020-05-08 14:10:52 -0700

    Remove node modules

--- .deploy.sh
+++ .deploy.sh
@@ -7,9 +7,9 @@ md_gen_path=~/md-notebook-gen
 website_path=~/website
 
 cd $md_notebook_path; ./indexer.sh
+cd $md_notebook_path; rm -rf node_modules
 cd $md_gen_path; npm install; ./generate_notebook.sh $md_notebook_path
 cd $website_path; rm -rf *
-cd $md_notebook_path; rm -rf node_modules
 cp -r $md_gen_path/output/* $website_path/
 cd $website_path; mv README.html index.html
 cd $website_path; git add .; git commit -m "Travis Autobuild"; git push origin master
