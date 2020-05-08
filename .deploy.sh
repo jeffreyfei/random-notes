ac4e6edc76bd97836819d2f4046a686d7ec041f4
Jeffrey Fei <jeffreyfei@msn.com>
2020-05-08 14:20:15 -0700

    Update deploy script

--- .deploy.sh
+++ .deploy.sh
@@ -6,8 +6,8 @@ md_notebook_path=/home/travis/build/jeffreyfei/random-notes
 md_gen_path=~/md-notebook-gen
 website_path=~/website
 
-cd $md_notebook_path; ./indexer.sh
 cd $md_notebook_path; rm -rf node_modules
+cd $md_notebook_path; ./indexer.sh
 cd $md_gen_path; npm install; ./generate_notebook.sh $md_notebook_path
 cd $website_path; rm -rf *
 cp -r $md_gen_path/output/* $website_path/
