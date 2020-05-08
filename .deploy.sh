4b9898ca340894384222943a1b33f14631817589
Jeffrey Fei <jeffreyfei@msn.com>
2020-05-08 13:52:06 -0700

    Update deploy descript

--- .deploy.sh
+++ .deploy.sh
@@ -9,6 +9,6 @@ website_path=~/website
 cd $md_notebook_path; ./indexer.sh
 cd $md_gen_path; npm install; ./generate_notebook.sh $md_notebook_path
 cd $website_path; rm -rf *
-cp $md_gen_path/output/* $website_path/
+cp -r $md_gen_path/output/* $website_path/
 cp $md_notebook_path/.index.html $website_path/index.html
 cd $website_path; git add .; git commit -m "Travis Autobuild"; git push origin master
